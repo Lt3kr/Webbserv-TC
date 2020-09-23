fccfc4700d2ced3973bb2b5c1ad83d6080838457
Lt3kr <70211742+Lt3kr@users.noreply.github.com>
2020-09-23 16:28:13 +0200

    Finally done it

--- EN_SERVER/app.js
+++ EN_SERVER/app.js
@@ -2,7 +2,23 @@ const express = require('express')
 const app = express()
 const port = 7676
 
+app.use(express.json())
+app.use(express.urlencoded())
 const clientDir = __dirname + "\\client\\"
 
 app.get('/', (req, res) => res.sendFile(clientDir + 'index.html'))
+app.get('/style', (req, res) => {
+  res.sendFile(clientDir + 'main_style.css')
+})
 app.listen(port, () => console.log(`Example app listening on port port!`))
+
+app.get('/gibba', (req, res) => {
+  res.sendFile(clientDir + 'cool_blixt.gif')
+})
+
+app.post('/sendMessage', function (req, res) {
+    console.log(req.body.name)
+    console.log(req.body.email)
+
+  res.redirect('/')
+})
